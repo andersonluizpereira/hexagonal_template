7e52044ff0b4be574a189666485f95b12ee311ce
ANDERSON PEREIRA <andy2903.alp@gmail.com>
2023-10-07 14:44:25 -0300

    controller
    feat: add controller customers

---
+++ app/src/main/java/com/comunidade/app/adapters/in/controller/CustomerController.java
@@ -0,0 +1,65 @@
+package com.comunidade.app.adapters.in.controller;
+
+import com.comunidade.app.adapters.in.controller.mapper.CustomerMapper;
+import com.comunidade.app.adapters.in.controller.request.CustomerRequest;
+import com.comunidade.app.adapters.in.controller.response.CustomerResponse;
+import com.comunidade.app.application.core.domain.Customer;
+import com.comunidade.app.application.ports.in.DeleteCustomerByIdInputPort;
+import com.comunidade.app.application.ports.in.FindCustomerByIdInputPort;
+import com.comunidade.app.application.ports.in.InsertCustomerInputPort;
+import com.comunidade.app.application.ports.in.UpdateCustomerInputPort;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/v1/customers")
+public class CustomerController {
+
+    @Autowired
+    private InsertCustomerInputPort insertCustomerInputPort;
+
+    @Autowired
+    private FindCustomerByIdInputPort findCustomerByIdInputPort;
+
+    @Autowired
+    private UpdateCustomerInputPort updateCustomerInputPort;
+
+    @Autowired
+    private DeleteCustomerByIdInputPort deleteCustomerByIdInputPort;
+
+    @Autowired
+    private CustomerMapper customerMapper;
+
+    @PostMapping
+    public ResponseEntity<Void> insert(@Valid @RequestBody CustomerRequest customerRequest) {
+        var customer = customerMapper.toCustomer(customerRequest);
+        insertCustomerInputPort.insert(customer, customerRequest.getZipCode());
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CustomerResponse> findById(@PathVariable final String id) {
+        var customer = findCustomerByIdInputPort.find(id);
+        var customerResponse = customerMapper.toCustomerResponse(customer);
+        return ResponseEntity.ok().body(customerResponse);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> update(@PathVariable final String id, @Valid @RequestBody CustomerRequest customerRequest) {
+        Customer customer = customerMapper.toCustomer(customerRequest);
+        customer.setId(id);
+        updateCustomerInputPort.update(customer, customerRequest.getZipCode());
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable final String id) {
+        deleteCustomerByIdInputPort.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
+
